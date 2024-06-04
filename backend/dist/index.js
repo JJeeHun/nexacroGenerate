ec1474c1ecdc9d4895470338cf5b2ef81fde5e52
JJeeHun <i-love-u_mylove@hanmail.net>
2024-06-05 02:59:24 +0900

    first

---
+++ backend/dist/index.js
@@ -0,0 +1,94 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mariadb_1 = __importDefault(require("mariadb"));
+const path_1 = __importDefault(require("path"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 5110;
+const schema = "board";
+const pool = mariadb_1.default.createPool({
+    host: "localhost",
+    user: "admin",
+    password: "admin88",
+    database: schema,
+    port: 3306,
+    bigNumberStrings: true,
+    bigIntAsNumber: true,
+    connectionLimit: 5,
+});
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/build")));
+let tableList;
+let tableInfo; // = {};
+const query = (query) => __awaiter(void 0, void 0, void 0, function* () {
+    let conn;
+    try {
+        conn = yield pool.getConnection();
+        return yield conn.query(query);
+    }
+    catch (err) {
+        return err;
+    }
+    finally {
+        if (conn)
+            conn.release();
+    }
+});
+const getTables = () => __awaiter(void 0, void 0, void 0, function* () {
+    return query(`SELECT 
+                        TABLE_NAME 
+                    FROM 
+                        INFORMATION_SCHEMA.TABLES 
+                    WHERE 
+                        TABLE_SCHEMA = '${schema}'`);
+});
+const getColumns = (tables) => __awaiter(void 0, void 0, void 0, function* () {
+    const unionQuery = tables
+        .map((table) => `SELECT 
+                            COLUMN_NAME, 
+                            DATA_TYPE, 
+                            CHARACTER_MAXIMUM_LENGTH, 
+                            IS_NULLABLE, 
+                            COLUMN_DEFAULT,
+                            '${table.TABLE_NAME}' as TABLE_NAME
+                        FROM 
+                            INFORMATION_SCHEMA.COLUMNS 
+                        WHERE 
+                            TABLE_SCHEMA = '${schema}' 
+                        AND TABLE_NAME = '${table.TABLE_NAME}'`)
+        .join("\r\nunion\r\n");
+    return query(unionQuery);
+});
+app.get("/list", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!tableList)
+        tableList = yield getTables();
+    if (!tableInfo) {
+        tableInfo = {};
+        const columns = yield getColumns(tableList);
+        columns.forEach((column) => {
+            tableInfo[column.TABLE_NAME] = tableInfo[column.TABLE_NAME] || [];
+            tableInfo[column.TABLE_NAME].push(column);
+        });
+    }
+    res.json({ table_list: tableList, table_info: tableInfo });
+}));
+app.get("*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "../../frontend/build", "index.html"));
+});
+app.listen(port, () => {
+    console.log(`Server Running on http://localhost:${port}`);
+});
---
+++ backend/src/index.ts
@@ -0,0 +1,109 @@
+import express from "express";
+import mariadb from "mariadb";
+import path from "path";
+import cors from "cors";
+import { createCipheriv } from "crypto";
+
+const app = express();
+const port = process.env.PORT || 5110;
+const schema = "board";
+
+const pool = mariadb.createPool({
+    host: "localhost",
+    user: "admin",
+    password: "admin88",
+    database: schema,
+    port: 3306,
+    bigNumberStrings: true,
+    bigIntAsNumber: true,
+    connectionLimit: 5,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
+
+type Table = {
+    TABLE_NAME: string;
+};
+
+type Tables = Table[];
+
+type Columns = {
+    COLUMN_NAME: string;
+    DATE_TYPE: string;
+    CHARACTER_MAXIMUM_LENGTH: number;
+    COLUMN_DEFAULT: string;
+    IS_NULLABLE: string;
+    TABLE_NAME: string;
+};
+
+let tableList: Tables;
+let tableInfo: any; // = {};
+
+const query = async (query: string) => {
+    let conn;
+
+    try {
+        conn = await pool.getConnection();
+
+        return await conn.query(query);
+    } catch (err) {
+        return err;
+    } finally {
+        if (conn) conn.release();
+    }
+};
+
+const getTables = async () => {
+    return query(`SELECT 
+                        TABLE_NAME 
+                    FROM 
+                        INFORMATION_SCHEMA.TABLES 
+                    WHERE 
+                        TABLE_SCHEMA = '${schema}'`);
+};
+
+const getColumns = async (tables: Tables) => {
+    const unionQuery = tables
+        .map(
+            (table) => `SELECT 
+                            COLUMN_NAME, 
+                            DATA_TYPE, 
+                            CHARACTER_MAXIMUM_LENGTH, 
+                            IS_NULLABLE, 
+                            COLUMN_DEFAULT,
+                            '${table.TABLE_NAME}' as TABLE_NAME
+                        FROM 
+                            INFORMATION_SCHEMA.COLUMNS 
+                        WHERE 
+                            TABLE_SCHEMA = '${schema}' 
+                        AND TABLE_NAME = '${table.TABLE_NAME}'`
+        )
+        .join("\r\nunion\r\n");
+
+    return query(unionQuery);
+};
+
+app.get("/list", async (req, res) => {
+    if (!tableList) tableList = await getTables();
+    if (!tableInfo) {
+        tableInfo = {};
+        const columns: Columns[] = await getColumns(tableList);
+
+        columns.forEach((column) => {
+            tableInfo[column.TABLE_NAME] = tableInfo[column.TABLE_NAME] || [];
+            tableInfo[column.TABLE_NAME].push(column);
+        });
+    }
+
+    res.json({ table_list: tableList, table_info: tableInfo });
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/build", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server Running on http://localhost:${port}`);
+});
